65fb864f2dee723fb91fe6ece9a99ad51ad9a6b8
michele minno <micheleminno@Air-di-michele.homenet.telecomitalia.it>
2022-02-08 17:06:10 +0100

    db scripts added, elastic search image re-added

---
+++ sql-test-setup/init-test.sql
@@ -0,0 +1,26 @@
+
+ALTER USER 'production'@'%' IDENTIFIED WITH mysql_native_password BY 'production';
+
+FLUSH PRIVILEGES;
+
+
+CREATE TABLE IF NOT EXISTS target (
+    id VARCHAR(255) PRIMARY KEY,
+    last_followers_page INT,
+    last_friends_page INT,
+    followers_next_cursor VARCHAR(255),
+    friends_next_cursor VARCHAR(255)
+)  ENGINE=INNODB;
+
+
+CREATE TABLE IF NOT EXISTS affinity (
+    id VARCHAR(255) PRIMARY KEY,
+    follows INT,
+    followed_by INT
+)  ENGINE=INNODB;
+
+INSERT INTO target (id, last_followers_page, last_friends_page, followers_next_cursor, friends_next_cursor)
+VALUES ('1', 15, 4, '123', '456');
+
+INSERT INTO target (id, last_followers_page, last_friends_page, followers_next_cursor, friends_next_cursor)
+VALUES ('4', 12, 15, '333', '444');
