48a0a922a1efd0505d93736ea29c5ad7c52a65b6
michele minno <micheleminno@Air-di-michele.homenet.telecomitalia.it>
2022-02-09 11:44:00 +0100

    elasticsearch url made explicit

--- express-server/server/interests.js
+++ express-server/server/interests.js
@@ -1,5 +1,5 @@
 const elasticsearch = require('elasticsearch');
-const client = new elasticsearch.Client();
+const client = new elasticsearch.Client({ node: 'http://localhost:9200' });
 
 const OK = 200;
 const NOK = 404;
